9e7afaae0102f5108e719172781541abf967f052
franco4457 <franco_forex@hotmail.com>
2023-05-14 12:49:53 -0300

    fix server types

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
+import { NextApiRequest } from "next"
 import { NextResponse } from "next/server"
 
-export function middleware(request: Request) {
-  const origin = request.headers.get('origin')
-  console.log(origin)
+export function middleware(request: NextApiRequest): NextResponse {
+  // const origin = request.headers?.get('origin')
+  // console.log(origin)
   const response = NextResponse.next()
   response.headers.set("Access-Control-Allow-Origin", "*")
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
--- server.ts
+++ server.ts
@@ -1,19 +1,20 @@
-import { middleware } from './middleware'
-import { createServer, IncomingMessage, ServerResponse } from 'http'
-import { parse } from 'url'
-import next from 'next'
+// 
+// import { middleware } from './middleware'
+// import { createServer } from 'http'
+// import { parse } from 'url'
+// import next, { NextApiRequest, NextApiResponse } from 'next'
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handler = app.getRequestHandler()
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handler = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req: IncomingMessage, res: ServerResponse) => {
-    const parsedUrl = parse(req.url || '', true)
-    middleware(req).handleRequest(req, res)
-    handler(req, res, parsedUrl)
-  }).listen(3000, (err: Error) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+// app.prepare().then(() => {
+//   createServer((req: NextApiRequest, res: NextApiResponse) => {
+//     const parsedUrl = parse(req.url || '', true)
+//     middleware(req).handleRequest(req, res)
+//     handler(req, res, parsedUrl)
+//   }).listen(3000, (err: Error): any => {
+//     if (err) throw err
+//     console.log('> Ready on http://localhost:3000')
+//   })
+// })
